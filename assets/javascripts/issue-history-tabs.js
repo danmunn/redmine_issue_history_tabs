e69f622f73d09b77109a44ba8316f999da132b62
Radovan Kepák <radovan@kepak.eu>
2013-01-14 13:32:32 +0100

    * fixes
    * fixed JS code
    + add position: fixed to tabs
    + add czech locale

--- assets/javascripts/issue-history-tabs.js
+++ assets/javascripts/issue-history-tabs.js
@@ -1,21 +1,46 @@
 /*
 Author: Daniel Munn <https://github.com/danmunn
+Author: Radovan Kepák <https://github.com/bckp
 Date: 23/05/2012
 */
-function init_tabs() {
-  tabComment = $('tab-history_comments');
-  tabAll = $('tab-history_all');
-  tabComment.observe('click', click_comments);
-  tabAll.observe('click', click_all);
-  tabComment.hasClassName('selected') ? show_comments() : show_all();
+function init_tabs(){
+  try{
+    tabComment = $('tab-history_comments');
+    tabAll = $('tab-history_all');
+    tabComment.observe('click', click_comments);
+    tabAll.observe('click', click_all);
+    tabComment.hasClassName('selected') ? show_comments() : show_all();
+		
+		a = $('tabs-scroll').cumulativeOffset();
+		
+		tabScroll = {
+			elm: tabScroll = $('tabs-scroll'),
+			left: a.left,
+			top: a.top,
+		};
+		
+		Event.observe(window, 'scroll', function(){
+			scroll_tabs();
+		});
+		
+		Event.observe(window, 'resize', function(){
+			resize_tabs();
+		});
+		
+		//Vyvolame je hned ze startu kdyby nahodou
+		scroll_tabs();
+		resize_tabs();
+  }catch(e){};
 }
 
 function show_comments() {
   $$('.journal').invoke('hide');
+	$$('.journal .details').invoke('hide');
   $$('.journal.has-notes').invoke('show');
 }
 
 function show_all() {
+	$$('.journal .details').invoke('show');
   $$('.journal').invoke('show');
 }
 
@@ -33,8 +58,26 @@ function click_all(e) {
   Event.stop(e);
 }
 
+function scroll_tabs(){
+	if( (document.viewport.getScrollOffsets().top - tabScroll.top) > 0 )
+		tabScroll.elm.setStyle({
+			'position': 'fixed',
+			'left': tabScroll.left + 'px',
+		});
+	else
+		tabScroll.elm.setStyle({
+			'position': 'absolute',
+			'left': '0px'
+		});
+}
+
+function resize_tabs(){
+	tabScroll.elm.setStyle({'width': $('content').getLayout().get('width') + 'px'});
+}
+
 var tabAll;
 var tabComment;
+var tabScroll;
 
 document.observe('dom:loaded', function(){
   init_tabs();
